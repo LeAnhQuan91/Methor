b4ca7184d86500395d045cc489773ffc6f0b2e43
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-04-11 19:26:25 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int c;
+        int d;
+        Shoting();
+        c = Add();
+        d = Sub(5,4,2);
+          }
+//Phuong thuc khong tham so va khong gia tri tra ve
+private static void Shoting()
+{
+    Console.WriteLine("Ban sung");
+}
+//Phuong thuc khong tham so va co gia tri tra ve
+private static int Add()
+{
+    return 20;
+}
+//Phuong thuc co gia tri tra ve va co tham so
+private static int Sub ( int a. int b, int c)
+{
+    return ((a-b)*c);
+}
+}
